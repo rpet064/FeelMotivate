074578f9b6fdc065e8201aba058607883f9dc9b7
rpet064 <93119631+rpet064@users.noreply.github.com>
2023-01-12 10:44:41 +1300

    update github actions to use node 16

--- Models/Notes/UpdateRequest.cs
+++ Models/Notes/UpdateRequest.cs
@@ -1,15 +1,17 @@
 namespace WebApi.Models.Notes;
 
+using System;
 using System.ComponentModel.DataAnnotations;
 using WebApi.Entities;
 
-public class UpdateRequest
-{
-    public string Content { get; set; }
 
-     private string ReplaceEmptyWithNull(string value)
+    public class UpdateRequest
     {
-        // replace empty string with null to make field optional
-        return string.IsNullOrEmpty(value) ? null : value;
+        public string Content { get; set; }
+
+        private string ReplaceEmptyWithNull(string value)
+        {
+            // replace empty string with null to make field optional
+            return string.IsNullOrEmpty(value) ? null : value;
+        }
     }
-}
